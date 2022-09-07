5f20ef56204e6293f09f903317ac75230ce4fda6
Клоков Андрей <Klokov.AI@parma.ru>
2022-09-07 14:37:55 +0300

    Создание учебного проекта

---
+++ src/main/java/ru/klokov/employeesdatasystem/EmployeesDataSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.klokov.employeesdatasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesDataSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesDataSystemApplication.class, args);
+    }
+
+}
