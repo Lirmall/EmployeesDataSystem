51b353b10648b4f5b3cde79db420edbe528f3e6f
Клоков Андрей <Klokov.AI@parma.ru>
2023-03-29 09:38:14 +0300

    Корректировка и дописывание тестов. Корректировка тестов, правки тестовых sql-скриптов

---
+++ src/test/resources/schemes/cleanDB/clean-DB.sql
@@ -0,0 +1,12 @@
+DROP SEQUENCE IF EXISTS employees_sequence;
+DROP TABLE IF EXISTS employees;
+DROP SEQUENCE IF EXISTS employee_position_range_sequence;
+DROP TABLE IF EXISTS employee_position_range;
+DROP SEQUENCE IF EXISTS ranges_sequence;
+DROP TABLE IF EXISTS ranges;
+DROP SEQUENCE IF EXISTS position_sequence;
+DROP TABLE IF EXISTS positions;
+DROP SEQUENCE IF EXISTS genders_sequence;
+DROP TABLE IF EXISTS genders;
+DROP SEQUENCE IF EXISTS worktypes_sequence;
+DROP TABLE IF EXISTS worktypes;
