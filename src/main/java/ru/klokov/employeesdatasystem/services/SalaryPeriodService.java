4d109f260fed41e006eb74c81fb67b821e242c6a
Клоков Андрей <Klokov.AI@parma.ru>
2023-04-19 14:11:08 +0300

    Наводим порядок в SalaryPeriodService. Рефакторинг

--- src/main/java/ru/klokov/employeesdatasystem/services/SalaryPeriodService.java
+++ src/main/java/ru/klokov/employeesdatasystem/services/SalaryPeriodService.java
@@ -5,7 +5,6 @@ import ru.klokov.employeesdatasystem.entities.SalaryPeriodEntity;
 import ru.klokov.employeesdatasystem.exceptions.PeriodsException;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,50 +77,6 @@ public class SalaryPeriodService {
     private int returnIntCountDaysOfMonth(LocalDate date) {
         boolean leapYear = date.getYear() % 4 == 0;
         return date.getMonth().length(leapYear);
-    }
-
-    private long countOfFullMonthBetween(LocalDate periodStart, LocalDate periodEnd) {
-        long fullMonthBetween;
-
-        LocalDate periodStartNextMonthWith1stDate = periodStart.plusMonths(1).withDayOfMonth(1);
-        LocalDate periodEnd1stDate = periodEnd.withDayOfMonth(1);
-        LocalDate periodEndPlus1Day = periodEnd.plusDays(1);
-
-        boolean perStartWith1stDate = periodStart.getDayOfMonth() == 1;
-        boolean perEndWithLastDate = periodEnd.getDayOfMonth() == periodEnd.lengthOfMonth();
-
-        if(periodStart.getYear() == periodEnd.getYear() && periodStart.getMonth() == periodEnd.getMonth()) {
-            if(periodStart.withDayOfMonth(periodStart.lengthOfMonth()).equals(periodEnd)) {
-                return 1L;
-            }
-            return 0L;
-        }
-
-        if(perStartWith1stDate && perEndWithLastDate) {
-            fullMonthBetween = countFullMonthWitStart1stDateAndEndLastDate(periodStart, periodEndPlus1Day);
-            return fullMonthBetween;
-        } else if (perStartWith1stDate) {
-            fullMonthBetween = countFullMonthWitStart1stDateAndEndLastDate(periodStart, periodEnd1stDate);
-            return fullMonthBetween;
-        } else if(perEndWithLastDate) {
-            fullMonthBetween =
-                    countFullMonthWitStart1stDateAndEndLastDate(periodStartNextMonthWith1stDate, periodEndPlus1Day);
-            return fullMonthBetween;
-        } else {
-            Period period = Period.between(periodStartNextMonthWith1stDate, periodEnd1stDate);
-            fullMonthBetween = period.toTotalMonths();
-            return fullMonthBetween;
-        }
-    }
-
-    private long countFullMonthWitStart1stDateAndEndLastDate(LocalDate start, LocalDate end) {
-        if (start.plusMonths(1).getYear() == end.getYear() && start.plusMonths(1).getMonth() == end.getMonth()) {
-            return 1L;
-        } else {
-            long result;
-            result = ChronoUnit.MONTHS.between(start, end.plusDays(1));
-            return result;
-        }
     }
 
     private int returnIntWorkdaysOnPeriod(SalaryPeriodEntity salaryPeriodEntity) {
