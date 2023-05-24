6c9299cac64896fc5f01bbc6a1d779d7ca6f26c1
Клоков Андрей <Klokov.AI@parma.ru>
2023-05-24 16:53:06 +0300

    Ввод-вывод (java.io). Excel - таблица работников. Заголовки

---
+++ src/main/java/ru/klokov/employeesdatasystem/headers/IHeader.java
@@ -0,0 +1,6 @@
+package ru.klokov.employeesdatasystem.headers;
+
+public interface IHeader {
+    String getName();
+    int getWidth();
+}
--- src/main/java/ru/klokov/employeesdatasystem/services/ExcelGeneratorService.java
+++ src/main/java/ru/klokov/employeesdatasystem/services/ExcelGeneratorService.java
@@ -1,5 +1,8 @@
 package ru.klokov.employeesdatasystem.services;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +12,7 @@ public class ExcelGeneratorService {
     private static final String SALARY_SHEET_NAME ="Salaries";
 
     public void generateEmployeesToExcel() {
-
+        Workbook workbook = new SXSSFWorkbook();
+        Sheet sheet = workbook.createSheet(EMPLOYEE_SHEET_NAME);
     }
  }
